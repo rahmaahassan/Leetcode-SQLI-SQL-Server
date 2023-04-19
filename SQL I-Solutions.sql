c77e9c7ed2b898512c6f0b1fe3a9ed89504e6c50
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-04-19 13:37:36 +0200

    Update SQL I-Solutions.sql

--- SQL I-Solutions.sql
+++ SQL I-Solutions.sql
@@ -14,6 +14,28 @@ Each row of this table gives information about the name of a country, the contin
 --
 
 SELECT name, population, area
-from world
-where area >= 3000000 or population >= 25000000
-order by name
+FROM world
+WHERE area >= 3000000 or population >= 25000000
+ORDER BY name
+
+--
+2)
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_id  | int     |
+| low_fats    | enum    |
+| recyclable  | enum    |
++-------------+---------+
+product_id is the primary key for this table.
+low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+
+Write an SQL query to find the ids of products that are both low fat and recyclable.
+Return the result table in any order.
+--
+
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' and recyclable = 'Y'
+ORDER BY product_id
