1e5dee0d8a8ff3182d27d79c69481df5ef10f0ab
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-04-20 10:00:15 +0200

    Update SQL I-Solutions.sql

--- SQL I-Solutions.sql
+++ SQL I-Solutions.sql
@@ -101,3 +101,27 @@ WHERE id not in (
     FROM orders
 )
 
+--
+5)
+Table: Employees
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the employee ID, employee name, and salary.
+
+Write an SQL query to calculate the bonus of each employee. 
+The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee name does not start with the character 'M'. 
+The bonus of an employee is 0 otherwise.
+Return the result table ordered by employee_id.
+--
+
+select employee_id, 
+case when employee_id%2 <>0 and name not like 'M%' then salary 
+else 0 end as bonus
+from employees
+order by employee_id
