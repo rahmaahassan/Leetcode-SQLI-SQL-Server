21967ce78976ef2c50766e3e8c34ad57505739f4
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-04-20 10:11:43 +0200

    Update SQL I-Solutions.sql

--- SQL I-Solutions.sql
+++ SQL I-Solutions.sql
@@ -125,3 +125,27 @@ case when employee_id%2 <>0 and name not like 'M%' then salary
 else 0 end as bonus
 from employees
 order by employee_id
+
+--
+6)
+Table: Salary
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| id          | int      |
+| name        | varchar  |
+| sex         | ENUM     |
+| salary      | int      |
++-------------+----------+
+id is the primary key for this table.
+The sex column is ENUM value of type ('m', 'f').
+The table contains information about an employee.
+ 
+Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) 
+with a single update statement and no intermediate temporary tables.
+Note that you must write a single update statement, do not write any select statement for this problem.
+--
+
+update salary
+set sex = case when sex = 'm' then 'f'
+           else 'm' end 
