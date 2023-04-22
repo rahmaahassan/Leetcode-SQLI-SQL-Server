3426b8ba95968a08135f696519fa446e22996761
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-04-22 11:43:15 +0200

    Update and rename SQL I-Solutions.sql to SQLI-Solutions.sql

--- SQLI-Solutions.sql
+++ SQLI-Solutions.sql
@@ -171,3 +171,22 @@ The final order of the Person table does not matter.
 delete A from Person A, Person B 
 where A.id > B.id and A.email=B.email
 
+--
+8)
+Table: Users
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+Return the result table ordered by user_id.
+--
+
+SELECT user_id, Upper(left(name, 1)) + lower(Substring(name, 2, len(name) -1)) name
+FROM users
+ORDER BY user_id
