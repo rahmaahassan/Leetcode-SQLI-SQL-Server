57096a650f109a873d66cd95cb0925a29d0450d4
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-04-22 11:56:36 +0200

    Update SQLI-Solutions.sql

--- SQLI-Solutions.sql
+++ SQLI-Solutions.sql
@@ -190,3 +190,26 @@ Return the result table ordered by user_id.
 SELECT user_id, Upper(left(name, 1)) + lower(Substring(name, 2, len(name) -1)) name
 FROM users
 ORDER BY user_id
+
+--
+9)
+Table Activities:
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key for this table, it may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
+ 
+Write an SQL query to find for each date the number of different products sold and their names.
+The sold products names for each date should be sorted lexicographically.
+Return the result table ordered by sell_date.
+--
+
+SELECT sell_date, count(product) num_sold,
+       string_agg(product, ',') products
+FROM (SELECT DISTINCT * from Activities) tabel1
+GROUP BY sell_date
+ORDER BY sell_date
