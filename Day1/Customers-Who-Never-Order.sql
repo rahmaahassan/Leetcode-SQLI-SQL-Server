b85f69b9ab81ff4645520685cd54cf4ac9da96ef
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-04-28 09:41:04 +0200

    Add files via upload

---
+++ Day1/Customers-Who-Never-Order.sql
@@ -0,0 +1,32 @@
+/*
+Table: Customers
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+
+Table: Orders
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+
+Write an SQL query to report all customers who never order anything.
+Return the result table in any order.
+*/
+
+SELECT name Customers
+FROM customers
+WHERE id not in (
+    SELECT customerID
+    FROM orders
+)
