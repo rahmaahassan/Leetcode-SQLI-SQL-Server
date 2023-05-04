76bcb3efc36471b8caf0553b858ff61c17e2c60e
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-04 14:29:04 +0300

    Update Rising-Temperature.sql

--- Day6/Rising-Temperature.sql
+++ Day6/Rising-Temperature.sql
@@ -1,39 +1,16 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset0 HelveticaNeue;\f2\fnil\fcharset0 Menlo-Regular;
-\f3\fnil\fcharset0 HelveticaNeue-Bold;}
-{\colortbl;\red255\green255\blue255;\red255\green255\blue255;\red30\green30\blue30;\red235\green237\blue244;
-\red255\green255\blue255;}
-{\*\expandedcolortbl;;\cssrgb\c100000\c100000\c100000;\cssrgb\c15686\c15686\c15686;\cssrgb\c93725\c94510\c96471\c74902;
-\cssrgb\c100000\c100000\c100000\c7059;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+/*
+Table: Weather
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
 
-\f0\fs24 \cf0 /*\
-\pard\pardeftab720\sa240\partightenfactor0
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+Return the result table in any order.
+*/
 
-\f1\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 Table:\'a0
-\f2\fs18 \cf4 \cb5 \strokec4 Weather
-\f1\fs28 \cf2 \cb3 \strokec2 \
-\pard\pardeftab720\sl300\partightenfactor0
-
-\f2 \cf4 \cb5 \strokec4 +---------------+---------+\
-| Column Name   | Type    |\
-+---------------+---------+\
-| id            | int     |\
-| recordDate    | date    |\
-| temperature   | int     |\
-+---------------+---------+\
-id is the primary key for this table.\
-This table contains information about the temperature on a certain day.\
-\pard\pardeftab720\sa240\partightenfactor0
-
-\f1 \cf2 \cb3 \strokec2 \'a0\
-Write an SQL query to find all dates'\'a0
-\f2\fs18 \cf4 \cb5 \strokec4 Id
-\f1\fs28 \cf2 \cb3 \strokec2 \'a0with higher temperatures compared to its previous dates (yesterday).\
-Return the result table in\'a0
-\f3\b any order
-\f1\b0 .\
-*/\
-}
