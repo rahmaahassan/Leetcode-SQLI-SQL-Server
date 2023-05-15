ae0f48d52d8548763f34a3a2e8675d1bf6644b71
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-15 13:54:35 +0300

    Update Duplicate-Emails.sql

--- Day10/Duplicate-Emails.sql
+++ Day10/Duplicate-Emails.sql
@@ -13,3 +13,7 @@ Write an SQL query to report all the duplicate emails. Note that it's guaranteed
 Return the result table in any order.
 */
 
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(*) > 1
