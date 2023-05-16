e2d7f42e61fb370ebaf72f617ffc68cc76985c15
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-16 10:08:25 +0300

    Update Bank-Account-Summary-II.sql

--- Day10/Bank-Account-Summary-II.sql
+++ Day10/Bank-Account-Summary-II.sql
@@ -28,3 +28,9 @@ Write an SQL query to report the name and balance of users with a balance higher
 The balance of an account is equal to the sum of the amounts of all transactions involving that account.
 Return the result table in any order.
 */
+
+SELECT name, SUM(amount) balance
+FROM Users u JOIN Transactions t
+ON u.account = t.account
+GROUP BY name
+HAVING SUM(amount) > 10000
